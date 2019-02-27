31a0ba3bf7fa9507ae52a250fdb144fed226d384
Henry Koehler <henryk1229@gmail.com>
2019-02-27 18:49:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves) { var arr = [];
+for (let i = 0, l = dwarves.length; i < l; i++) {
+  
+  arr.push(`${arr.length + 1}. ${dwarves[i]}`); 
+  }
+  
+ return `${arr.join(' ')} `;
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls){ var arr = [];
+for (let i = 0, l = planeteerCalls.length; i < l; i++) {
+  
+  arr.push(`${planeteerCalls[i]}!`); 
+  
+  for (let i = 0, l = arr.length; i < l; i++) {
+    
+    arr[i] = arr[i].toUpperCase();
+  }
 }
 
-function longPlaneteerCalls(words) {
+return arr;
+}
+
+function longPlaneteerCalls(words) { 
+  if (words.length > 3) { 
+    
+    return true;
+  } else {
+    
+    return false;
+  }
 }
 
 function findTheCheese (foods) {
-}
+  for (let i = 0, l = foods.length; i < l; i++) {
+    if (foods[i] === "gouda") {
+   
+      return "gouda";
+     } else if (foods[i] === "cheddar") {
+       
+        return "cheddar";
+       } else if (foods[i] === "camembert") {
+         
+          return "camembert";
+        }
+      }
+     return "no cheese!";
+    }
+  
+
