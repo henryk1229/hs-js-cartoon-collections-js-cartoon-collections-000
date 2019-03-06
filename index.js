5f0e65066692f59c5b2c67caf60b6f20b46a7256
Henry Koehler <henryk1229@gmail.com>
2019-03-06 17:30:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,50 +1,39 @@
-function dwarfRollCall(dwarves) { var arr = [];
-for (let i = 0, l = dwarves.length; i < l; i++) {
-  
-  arr.push(`${arr.length + 1}. ${dwarves[i]}`); 
+function dwarfRollCall(dwarves) { 
+  var arr = [];
+  for (let i = 0, l = dwarves.length; i < l; i++) {
+    arr.push(`${arr.length + 1}. ${dwarves[i]}`); 
   }
-  
- return `${arr.join(' ')} `;
+  return `${arr.join(' ')} `;
 }
 
-function summonCaptainPlanet(planeteerCalls){ var arr = [];
-for (let i = 0, l = planeteerCalls.length; i < l; i++) {
-  
-  arr.push(`${planeteerCalls[i]}!`); 
-  
-  for (let i = 0, l = arr.length; i < l; i++) {
-    
-    arr[i] = arr[i].toUpperCase();
+function summonCaptainPlanet(planeteerCalls){ 
+  var arr = [];
+  for (let i = 0, l = planeteerCalls.length; i < l; i++) {
+    arr.push(`${planeteerCalls[i].toUpperCase()}!`); 
   }
-}
-
-return arr;
+  return arr;
 }
 
 function longPlaneteerCalls(words) { 
-  if (words.length > 3) { 
-    
-    return true;
-  } else {
-    
-    return false;
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true;
+    }
   }
+  return false;
 }
 
 function findTheCheese (foods) {
   for (let i = 0, l = foods.length; i < l; i++) {
     if (foods[i] === "gouda") {
-   
       return "gouda";
-     } else if (foods[i] === "cheddar") {
-       
-        return "cheddar";
-       } else if (foods[i] === "camembert") {
-         
-          return "camembert";
-        }
-      }
-     return "no cheese!";
+    } else if (foods[i] === "cheddar") {
+      return "cheddar";
+    } else if (foods[i] === "camembert") {
+      return "camembert";
     }
+  }
+ return "no cheese!";
+}
   
 
